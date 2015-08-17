e194350afbf74cdb3dfde5214f48be023c7e7424
Bryan Deeney <bdeeney@pobox.com>
2015-08-17 10:59:08 -0400

    Add additional package info to setup.py

--- setup.py
+++ setup.py
@@ -12,14 +12,21 @@ def read_release_version():
     with open("RELEASE-VERSION", "r") as f:
         return f.readline().strip()
 
+url = 'https://github.com/bdeeney/PikaChewie'
+version = read_release_version()
+
 setup(
     name='PikaChewie',
+    version=version,
     description='A pika-based RabbitMQ publisher-consumer framework',
     long_description='''PikaChewie is your pika co-pilot, providing RabbitMQ
         messaging tools with bandoliers included.''',
     author='Bryan Deeney',
     author_email='rennybot@pobox.com',
     packages=find_packages(exclude=['tests.*', 'tests']),
+    url=url,
+    download_url='{url}/tarball/{version}'.format(url=url, version=version),
+    license='BSD',
     test_suite='nose.collector',
     include_package_data=True,
     zip_safe=False,
@@ -28,5 +35,5 @@ setup(
         'simplejson',
         'tornado',
     ],
-    version=read_release_version(),
+    classifiers=[],
 )
